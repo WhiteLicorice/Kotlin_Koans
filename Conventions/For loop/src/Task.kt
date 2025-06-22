0017fa89aab6bd8f8e0520ddc9cae5c95097ef91
Rene Andre Bedonia Jocsing <rbjocsing129@gmail.com>
2025-06-22 10:36:39 +0800

    task: finished conventions

--- Conventions/For loop/src/Task.kt
+++ Conventions/For loop/src/Task.kt
@@ -1,4 +1,18 @@
-class DateRange(val start: MyDate, val end: MyDate)
+class DateRange(val start: MyDate, val end: MyDate) : Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> {
+        return object : Iterator<MyDate> {
+            var curr = start
+
+            override fun hasNext(): Boolean = curr <= end
+
+            override fun next(): MyDate {
+                val res = curr
+                curr = curr.followingDate()
+                return res
+            }
+        }
+    }
+}
 
 fun iterateOverDateRange(firstDate: MyDate, secondDate: MyDate, handler: (MyDate) -> Unit) {
     for (date in firstDate..secondDate) {
--- Conventions/Operators overloading/src/Task.kt
+++ Conventions/Operators overloading/src/Task.kt
@@ -2,15 +2,29 @@ import TimeInterval.*
 
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
 
-// Supported intervals that might be added to dates:
 enum class TimeInterval { DAY, WEEK, YEAR }
 
-operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = TODO()
+// Support adding a single TimeInterval
+operator fun MyDate.plus(interval: TimeInterval): MyDate {
+    val defaultIncrement = 1
+    return addTimeIntervals(interval, defaultIncrement)
+}
+
+// Support adding repeated intervals (like YEAR * 2)
+data class RepeatedTimeInterval(val timeInterval: TimeInterval, val multiplier: Int)
+
+operator fun TimeInterval.times(times: Int): RepeatedTimeInterval {
+    return RepeatedTimeInterval(this, times)
+}
+
+operator fun MyDate.plus(repeat: RepeatedTimeInterval): MyDate {
+    return addTimeIntervals(repeat.timeInterval, repeat.multiplier)
+}
 
 fun task1(today: MyDate): MyDate {
     return today + YEAR + WEEK
 }
 
 fun task2(today: MyDate): MyDate {
-    TODO("Uncomment") //return today + YEAR * 2 + WEEK * 3 + DAY * 5
+    return today + YEAR * 2 + WEEK * 3 + DAY * 5
 }
